7b7caddc181994f18f9e8136b28a7645030e95cf
DevBotDiscord <102542986+DevBotDiscord@users.noreply.github.com>
2024-10-20 17:05:22 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'Duy Uyên:3';
+const giftUrl = 'https://zalo.me/0866594087';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng ebe Duy Uyên';
+const contentCard = 'Chúc mừng 20-10, Béo he yêu dấu! 💐Nhân ngày đặc biệt này, anh chỉ muốn nói rằng anh rất biết ơn vì có em trong cuộc đời, Em là người con gái tuyệt vời nhất mà anh từng gặp, luôn mang đến cho anh niềm vui và hạnh phúc, Anh hứa sẽ luôn yêu thương, chăm sóc và bảo vệ em, Dù có ở xa, nhưng tình cảm anh dành cho em luôn gần gũi và ấm áp, Chúc em ngày 20-10 tràn đầy niềm vui, hạnh phúc và luôn tươi cười như những đóa hoa xinh đẹp, Yêu em rất nhiều! ❤️'
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'embe.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
